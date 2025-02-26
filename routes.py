44943aecf5c1b88f2ce4c952816238e27b5a829f
Luiz Fernando <luizfernandoluth@gmail.com>
2025-02-26 08:49:47 -0300

    finished

--- routes.py
+++ routes.py
@@ -14,14 +14,14 @@ from dotenv import load_dotenv
 load_dotenv()  # Carrega as variáveis do .env
 
 config = {
-    "apiKey": os.getenv("APIKEY"),
-    "authDomain": os.getenv("AUTHDOMAIN"),
-    "projectId": os.getenv("PROJECTID"),
-    "storageBucket": os.getenv("STORAGEBUCKET"),
-    "messagingSenderId": os.getenv("MESSAGINGSENDERID"),
-    "appId": os.getenv("APPID"),
-    "measurementId": os.getenv("MEASUREMENTID"),
-    "databaseURL": os.getenv("DATABASEURL")
+    "apiKey": os.getenv("API_KEY"),
+    "authDomain": os.getenv("AUTH_DOMAIN"),
+    "projectId": os.getenv("PROJECT_ID"),
+    "storageBucket": os.getenv("STORAGE_BUCKET"),
+    "messagingSenderId": os.getenv("MESSAGING_SENDER_ID"),
+    "appId": os.getenv("APP_ID"),
+    "measurementId": os.getenv("MEASUREMENT_ID"),
+    "databaseURL": os.getenv("DATABASE_URL")
 }
 
 firebase = pyrebase.initialize_app(config)
